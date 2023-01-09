9a88f045b1f45e5a0a24fab256e7c7e7421534b2
Eric <eric.bracamonte@utec.edu.pe>
2023-01-09 13:40:49 -0500

    initial click function

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function click(e){
+    console.log(this);
+
+}
+
+
+let botones = document.querySelectorAll('.button');
+
+
+
+
+for(but of botones){
+
+    but.addEventListener('click',click);
+
+}
